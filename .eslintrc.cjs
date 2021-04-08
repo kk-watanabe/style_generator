1198bd8617278be3db6ebbee5b690792126c2c5a
kk-watanabe <kazuki.watanabe@bebit.com>
2021-04-08 20:19:22 +0900

    feat: Add jest setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,18 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  overrides: [
+    { files: ['*.svelte'], processor: 'svelte3/svelte3' },
+    {
+      files: [
+        "test/_helpers/*.{j,t}s",
+        "test/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ],
 	settings: {
 		'svelte3/typescript': require('typescript')
 	},
@@ -16,5 +27,5 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
-	}
+  }
 };
