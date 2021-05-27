03e11aed0b8636c8bd2030ca683ea93ac195fe95
kk-watanabe <kazuki.watanabe@bebit.com>
2021-05-27 16:24:51 +0900

    fix: Update css order

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,34 +15,34 @@
 
 <style lang="scss">
   main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
+      margin: 0 auto;
+      padding: 1em;
+      text-align: center;
+    }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 4rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 2rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
     h1 {
-      max-width: none;
+      margin: 4rem auto;
+      max-width: 14rem;
+      color: #ff3e00;
+      text-transform: uppercase;
+      font-weight: 100;
+      font-size: 4rem;
+      line-height: 1.1;
     }
 
     p {
-      max-width: none;
+      margin: 2rem auto;
+      max-width: 14rem;
+      line-height: 1.35;
     }
-  }
+
+    @media (min-width: 480px) {
+      h1 {
+        max-width: none;
+      }
+
+      p {
+        max-width: none;
+      }
+    }
 </style>
