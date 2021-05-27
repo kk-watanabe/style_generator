f58f9239e86245821548d86199d4ca45f98948d6
kk-watanabe <kazuki.watanabe@bebit.com>
2021-05-27 16:25:17 +0900

    feat: Add ColorPickerCanvas components

---
+++ src/lib/form/color-picker/ColorPickerCanvas.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { clamp } from "$util/color-util";
+  import { Colors } from "$models/Colors";
+
+  export let colors: Colors;
+
+  const defaultClass = "color-picker-canvas";
+
+  // canvas
+  const canvasWidth = 300;
+  const canvasHeight = 150;
+
+  // coursor
+  const cursorSize = 10;
+
+  let canvas: HTMLCanvasElement;
+  let context: CanvasRenderingContext2D;
+
+  let isMouseDown = false;
+
+  $: cursorStyle = () => {
+    const cursorCenter = cursorSize / 2;
+    const hsva = colors.hsva;
+    const x = hsva.s * parseInt(`${canvasWidth}`, cursorSize) - cursorCenter;
+    const y =
+      (1 - hsva.v) * parseInt(`${canvasHeight}`, cursorSize) - cursorCenter;
+
+    return `width: ${cursorSize}px; height: ${cursorSize}px; transform: translate(${x}px, ${y}px)`;
+  };
+
+  onMount(() => {
+    context = canvas.getContext("2d");
+
+    const baseGradient = context.createLinearGradient(0, 0, canvasWidth, 0);
+    baseGradient.addColorStop(0, "hsla(0, 0%, 100%, 1)"); // white
+    baseGradient.addColorStop(1, `hsla(${colors.hsla.h}, 100%, 50%, 1)`);
+    context.fillStyle = baseGradient;
+    context.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    const coverGradient = context.createLinearGradient(0, 0, 0, canvasHeight);
+    coverGradient.addColorStop(0, "hsla(0, 0%, 100%, 0)"); // transparent
+    coverGradient.addColorStop(1, "hsla(0, 0%, 0%, 1)"); // black
+    context.fillStyle = coverGradient;
+    context.fillRect(0, 0, canvasWidth, canvasHeight);
+  });
+
+  const updateColors = (e: MouseEvent) => {
+    const { width, height } = canvas.getBoundingClientRect();
+
+    colors = Colors.convertHSVAToColors({
+      h: colors.hue,
+      s: clamp(e.offsetX, 0, width) / width,
+      v: 1 - clamp(e.offsetY, 0, height) / height,
+      a: 1,
+    });
+  };
+
+  const onClick = (e: MouseEvent) => {
+    updateColors(e);
+  };
+
+  const onMouseMove = (e: MouseEvent) => {
+    if (isMouseDown) {
+      updateColors(e);
+    }
+  };
+
+  const onMouseUp = () => {
+    isMouseDown = false;
+
+    window.removeEventListener("mouseup", onMouseUp);
+  };
+
+  const onMouseDown = (e: MouseEvent) => {
+    e.preventDefault();
+
+    isMouseDown = true;
+
+    window.addEventListener("mouseup", onMouseUp);
+  };
+</script>
+
+<div class={defaultClass}>
+  <canvas
+    bind:this={canvas}
+    width={canvasWidth}
+    height={canvasHeight}
+    class={`${defaultClass}__palette`}
+    on:click={onClick}
+    on:mousedown={onMouseDown}
+    on:mousemove={onMouseMove}
+  />
+
+  <div class={`${defaultClass}__cursor`} style={cursorStyle()} />
+</div>
+
+<style lang="scss">
+  @import "../../../styles/_color.scss";
+
+  .color-picker-canvas {
+    position: relative;
+    display: inline-block;
+    overflow: hidden;
+    cursor: pointer;
+
+    &__cursor {
+      position: absolute;
+      top: 0;
+      left: 0;
+      border: 1px solid $isColorBase800;
+      border-radius: 50%;
+      box-shadow: 0 0 0 1px $isColorWhite;
+    }
+  }
+</style>
---
+++ src/stories/form/color-picker/ColorPickerCanvas.stories.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import { Colors } from "$models/Colors";
+  import ColorPickerCanvas from "$lib/form/color-picker/ColorPickerCanvas.svelte";
+
+  let colors = Colors.convertHexToColors("#115EFF");
+</script>
+
+<Meta
+  title="Form/color-picker/ColorPickerCanvas"
+  component={ColorPickerCanvas}
+/>
+
+<Template let:args>
+  <ColorPickerCanvas bind:colors />
+
+  <div style={`width: 50px; height: 50px; background-color: ${colors.hex};`} />
+</Template>
+
+<Story name="Default" />
