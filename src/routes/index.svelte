038e8d7f04fdee8a04100fe19b247ec8be7abf51
kk-watanabe <kazuki.watanabe@bebit.com>
2021-06-21 16:39:16 +0900

    fix: Delete Counter components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-  import Counter from "$lib/Counter.svelte";
 </script>
 
 <main>
   <h1>Hello world!</h1>
-
-  <Counter />
 
   <p>
     Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
@@ -15,34 +12,34 @@
 
 <style lang="scss">
   main {
-      margin: 0 auto;
-      padding: 1em;
-      text-align: center;
-    }
+    margin: 0 auto;
+    padding: 1em;
+    text-align: center;
+  }
 
+  h1 {
+    margin: 4rem auto;
+    max-width: 14rem;
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-weight: 100;
+    font-size: 4rem;
+    line-height: 1.1;
+  }
+
+  p {
+    margin: 2rem auto;
+    max-width: 14rem;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
     h1 {
-      margin: 4rem auto;
-      max-width: 14rem;
-      color: #ff3e00;
-      text-transform: uppercase;
-      font-weight: 100;
-      font-size: 4rem;
-      line-height: 1.1;
+      max-width: none;
     }
 
     p {
-      margin: 2rem auto;
-      max-width: 14rem;
-      line-height: 1.35;
+      max-width: none;
     }
-
-    @media (min-width: 480px) {
-      h1 {
-        max-width: none;
-      }
-
-      p {
-        max-width: none;
-      }
-    }
+  }
 </style>
